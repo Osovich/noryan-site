ca24424bdb50df410cd6677edace38077e037dd2
Franco Jiménez <yokoroz@gmail.com>
2025-09-22 00:26:25 +1000

    First steps on database and core design

---
+++ app/utils/supabase/client.ts
@@ -0,0 +1,9 @@
+import { createBrowserClient } from '@supabase/ssr'
+
+export function createClient() {
+  // Create a browser client that can read and write cookies
+  return createBrowserClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+  )
+}
